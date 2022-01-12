2522cf2e2463faba9e37d827f445ff2bbf3f4be1
mor0666 <81852929+mor0666@users.noreply.github.com>
2022-01-12 23:55:03 +0200

    improved MAP@40 and Retrieval time

--- new_back_end_search.py
+++ new_back_end_search.py
@@ -1,3 +1,6 @@
+import math
+from contextlib import closing
+
 import nltk
 # from google.cloud import storage
 import re
@@ -5,6 +8,9 @@ import pickle
 from nltk.corpus import stopwords
 from pathlib import Path
 
+from inverted_index_gcp import MultiFileReader
+
+#tokenize function
 nltk.download('stopwords')
 english_stopwords = frozenset(stopwords.words('english'))
 corpus_stopwords = ["category", "references", "also", "external", "links",
@@ -19,38 +25,64 @@ RE_WORD = re.compile(r"""[\#\@\w](['\-]?\w){,24}""", re.UNICODE)
 def tokenize(text):
     return [token.group() for token in RE_WORD.finditer(text.lower())]
 
-# def load():
-#     bucket_name = 'newbodysearch'
-#     client = storage.Client()
-#     for blob in client.list_blobs(bucket_name):
-#         if not blob.name.endswith('page_view.pickle'):
-#             continue
-#         with blob.open("rb") as file:
-#             PageViewDT = pickle.load(file)
-#     for blob in client.list_blobs(bucket_name):
-#         if not blob.name.endswith('page_rank.pickle'):
-#             continue
-#         with blob.open("rb") as file:
-#             PageRankD = pickle.load(file)
-#     for blob in client.list_blobs(bucket_name):
-#         if not blob.name.endswith('id_to_title.pickle'):
-#             continue
-#         with blob.open("rb") as file:
-#             ID_Title_dict = pickle.load(file)
-#     for blob in client.list_blobs(bucket_name):
-#         if not blob.name.endswith('postings_gcp/index.pickle'):
-#             continue
-#     for blob in client.list_blobs(bucket_name):
-#         if not blob.name.endswith('DL_dict_DL_dict.pickle'):
-#             continue
-#     dict_DL= read_index('DL_dict_DL_dict')
-#     dict_title= read_index('id_to_title')
-#     page_view_data_frame= read_index('page_view')
-#     page_rank_dict= read_index('page_rank')
-#     idx_body= read_index('index')
-#     return dict_title, page_view_data_frame, page_rank_dict, idx_body, dict_DL
 
+TUPLE_SIZE = 6
+#reading the post list from the index of specific word
+def read_posting_list(inverted, w):
+    with closing(MultiFileReader()) as reader:
+        locs = inverted.posting_locs[w]
+        b = reader.read(locs, inverted.df[w] * TUPLE_SIZE)
+        posting_list = []
+        for i in range(inverted.df[w]):
+            doc_id = int.from_bytes(b[i * TUPLE_SIZE:i * TUPLE_SIZE + 4], 'big')
+            tf = int.from_bytes(b[i * TUPLE_SIZE + 4:(i + 1) * TUPLE_SIZE], 'big')
+            posting_list.append((doc_id, tf))
+        return posting_list
 
+#calculate cosine similarity using tf-idf
+def cossim(query, index, dict_Dl):
+    result_dict = {}
+    query_vector = {}
+    len_q= len(query)
+    for idDoc in dict_Dl.keys():
+        result_dict[idDoc]=float(0)
+    for word in query: #make query as vector
+        if(word not in query_vector):
+            query_vector[word] =0
+        query_vector[word] += 1 / len_q
+    temp_vec_q= query_vector.values()
+    norm=0.0
+    for i in temp_vec_q: #normalize the query
+        norm= norm+i**2
+    norm_r = math.sqrt(norm)
+    word_tf_dict={}
+    inner=0.0
+    for term in query: #calculate cossim
+        try:
+            idf = math.log2(len(dict_Dl) / index.df[term])
+        except:
+            idf=0
+        tf_word = query_vector[term]
+        try:
+            pls = read_posting_list(index, term)
+            pls=pls
+            # print(pls)
+        except:
+            pls=[]
+        for id, tf in pls:
+            result_dict[id]+= idf*tf
+            try:
+                word_tf_dict[id][term]=tf/dict_Dl[id]
+            except:
+                g=[]
+        for k, v in word_tf_dict.items():
+            for key, val in v.items():
+                inner+=query_vector[term]*word_tf_dict.get(key)
+                inner = idf*inner
+            result_dict[id] = (inner / (dict_Dl[id] * norm_r)) * (len(word_tf_dict))
+    return result_dict
+
+  #read index
 def read_index(base_dir, name):
     with open(Path(base_dir) / f'{name}.pkl', 'rb') as f:
         return pickle.load(f)
@@ -59,26 +91,3 @@ def read_index(base_dir, name):
 def get_posting_gen(index, word):
     words, pls = zip(index.read_posting_list(word))
     return (words, pls)
-
-def cossim(query, index, dict_Dl):
-    result_dict = {}
-    query_vector = {}
-    for idDoc in dict_Dl.keys():
-        result_dict[idDoc]=float(0)
-    for word in query:
-        query_vector[word] = query.count(word)
-    for term in query:
-        try:
-            words, pls = get_posting_gen(index, term)
-            pls_post = pls[0]
-        except:
-            pls_post=[]
-        for id, tf in pls_post:
-            result_dict[id] += query_vector[term] * tf
-    for id in dict_Dl.keys():
-        result_dict[id] = result_dict[id] * (1/len(query) * (1/dict_Dl[id]))
-    return result_dict
-
-
-# def get_top_n(sim_dict, N=3):
-#     return sorted([(doc_id, round(score, 5)) for doc_id, score in sim_dict.items()], key=lambda x: x[1], reverse=True)[:N]
